fd65110e8ebf859031e11eb55e3bffb54af246d6
Kushal Shingote <kushalshingote2@gmail.com>
2022-02-02 17:46:57 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#    Copyright 2020 Connor Ferster
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+"""
+Converts your Python calculation script into beautifully rendered Latex, 
+similar to how you would write a calculation by hand.
+"""
+__version__ = "1.4.1" #
+from .decorator import handcalc
+__all__ = ["handcalc"]
+
---
+++ about.md
@@ -0,0 +1,15 @@
+## This is the about pages
+
+<p>
+  <img src="https://travis-ci.org/connorferster/handcalcs.svg?branch=master">
+<a href='https://coveralls.io/github/connorferster/handcalcs?branch=master'><img src='https://coveralls.io/repos/github/connorferster/handcalcs/badge.svg?branch=master' alt='Coverage Status' /></a>
+  <img src="https://img.shields.io/badge/code%20style-black-000000.svg">
+  <img src="https://img.shields.io/pypi/v/handcalcs">
+  <img src="https://img.shields.io/pypi/pyversions/handcalcs">
+  <img src="https://img.shields.io/github/license/connorferster/handcalcs">
+  <img src="https://img.shields.io/pypi/dm/handcalcs">
+</p>
+<p align="center">
+  <img src="../images/handcalcs.jpg"><br>
+  Covert art by <a href = "https://www.instagram.com/joshuahoibergtattoos/">Joshua Hoiberg</a>
+</p>
